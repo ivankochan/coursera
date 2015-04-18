b5af2dadb89e6be548ae71a3d54f62e8d3f1c5dc
Ivan Kochan <ivankochan83@gmail.com>
2015-04-18 12:41:28 +0200

    Init commit

---
+++ ReactiveProgramming/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -0,0 +1,23 @@
+package quickcheck
+
+import java.util.Random
+
+import common._
+
+import org.scalacheck._
+import Arbitrary._
+import Gen._
+import Prop._
+
+abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
+
+  property("min1") = forAll { a: Int =>
+    val h = insert(a, empty)
+    findMin(h) == a
+  }
+
+  lazy val genHeap: Gen[H] = ???
+
+  implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
+
+}
